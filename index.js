4d48be069f2ce14b44c529b34b6932b3544f682b
blakerutledge <blake@blakerutledge.com>
2022-03-22 18:18:52 -0600

    doing it manually

--- index.js
+++ index.js
@@ -1,20 +1,61 @@
+const fs = require('fs')
+const path = require('path')
 const { Transformer } = require("@parcel/plugin")
 const Handlebars  = require("handlebars");
-const inlinesvg = require("handlebars-helper-svg");
-Handlebars.registerHelper('svg', inlinesvg);
 
 const transformer = new Transformer({
   async transform({ asset }) {
-    const content = await asset.getCode();
+
+    let content = await asset.getCode();
+
+
+    // INLINE svg assets
+
+    let regex = /<include src=(.*?)\/>/g;
+    let includes = content.match( regex );
+
+    let cache = {}
+
+    for ( let match in includes ) {
+
+      let file = includes[ match ]
+
+      file = file.replace( /<include src=/g, "" )
+        .replace( /\/>/g, "" )
+        .replace( /"/g, "" )
+        .replace( /'/g, "" );
+
+      // HACK this should be better but it works for me
+      file = path.join( __dirname,  '..', '..', 'src', 'frontend', file )
+      
+      let svg = ( cache[ file ] )
+        ? cache[ file ]
+        : fs.readFileSync( file, 'utf-8' )
+
+
+      cache[ file ] = ( cache[ file ] )
+        ? cache[ file ]
+        : svg
+
+      content = content.replace( includes[ match ], cache[ file ] )
+
+    }
+
+
     const precompiled = Handlebars.precompile(content);
 
+
     asset.setCode(`
-    import Handlebars from 'handlebars';
+    import Handlebars from 'handlebars/dist/handlebars.runtime';
     const templateFunction = Handlebars.template(${precompiled});
     export default templateFunction`)
     asset.type = "js"
     return [asset];
   },
 });
+
+
+
+
 
 module.exports = transformer;
