24c239aad2b9c1b8551f61d1bd496d1cce166fe6
Shelby <pedrohenriqueissa@gmail.com>
2024-09-18 18:06:31 -0300

    update

--- index.js
+++ index.js
@@ -163,7 +163,7 @@ module.exports = new Transformer({
               return new Handlebars.SafeString(`{{${name}}}`);
             }
           }
-        },waxwax
+        },
       });
       const partialsDir = path.join(
         projectRoot,
